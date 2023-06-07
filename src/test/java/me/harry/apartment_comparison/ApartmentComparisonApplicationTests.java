ef6eac12853ee8befeb25fdc65ae316a33009415
hsw0905 <hsw0905@gmail.com>
2023-06-07 22:01:28 +0900

    spring boot init

---
+++ src/test/java/me/harry/apartment_comparison/ApartmentComparisonApplicationTests.java
@@ -0,0 +1,13 @@
+package me.harry.apartment_comparison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentComparisonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
